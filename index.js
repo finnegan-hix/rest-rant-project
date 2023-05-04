aa1054902a8c75751985ea09b1ed0b4965f63f3c
Finngodumb <finny12349@gmail.com>
2023-05-03 22:49:39 -0400

    Adding 404 page view

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 page</h1>')
+    res.render(404).send('<h1>404 page</h1>')
 })
 app.listen(process.env.PORT)
