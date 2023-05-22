6a38b8ec1aeaf2db4c357249b70c2c4c7acfca9d
Finngodumb <finny12349@gmail.com>
2023-05-22 14:54:09 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -41,6 +41,21 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('POST /places')
 })
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+
+
 
 
 
